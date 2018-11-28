447dfe8b1d50534b2634d61e0e6b017e69473195
Mailz95 <verwolfik@mail.ru>
2018-11-28 20:10:52 +0300

    VSE NORM

--- SecScript.py
+++ SecScript.py
@@ -1,7 +1,7 @@
 def mF(a):
     for i in range (10):
         if i == 3 or i==5:
-            print('xyi')
+            print('vse')
         else:
-            print('zbs')
+            print('norm')
 mF(10)
